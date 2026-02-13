c160740c8db68d8bd0c10e7ccb3161c90b173b96
RefkiSeptian <refkiseptianrs@gmail.com>
2026-02-13 17:17:39 +0700

    test/vector-dotproduct

--- src/test/java/com/example/individualprep/IndividualprepApplicationTests.java
+++ src/test/java/com/example/individualprep/IndividualprepApplicationTests.java
@@ -2,6 +2,8 @@ package com.example.individualprep;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.individualprep.service.ArithmeticUtility;
+import com.example.individualprep.service.VectorUtility;
 
 @SpringBootTest
 class IndividualprepApplicationTests {
@@ -9,5 +11,27 @@ class IndividualprepApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void testArithmeticUtilityBean() {
+		ArithmeticUtility arithmeticUtility = new ArithmeticUtility();
+		assert arithmeticUtility != null;
+	}
+	
+	@Test
+	void testVectorUtilityBean() {
+		VectorUtility vectorUtility = new VectorUtility();
+		assert vectorUtility != null;
+	}
+
+	@Test
+	void testVectordotProduct() {
+		VectorUtility vectorUtility = new VectorUtility();
+		double[] v1 = {1.0, 2.0, 3.0};
+		double[] v2 = {4.0, 5.0, 6.0};
+		double result = vectorUtility.dotProduct(v1, v2);
+		assert result == 32.0; 
+	}
+	
 
 }
