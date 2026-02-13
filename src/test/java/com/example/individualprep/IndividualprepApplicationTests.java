5a6ed0d12094e7eeaa38658bee87b804e80eca0f
RefkiSeptian <refkiseptianrs@gmail.com>
2026-02-13 17:14:35 +0700

    test/arithmetic-subtract

--- src/test/java/com/example/individualprep/IndividualprepApplicationTests.java
+++ src/test/java/com/example/individualprep/IndividualprepApplicationTests.java
@@ -2,12 +2,41 @@ package com.example.individualprep;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.individualprep.service.ArithmeticUtility;
+import com.example.individualprep.service.VectorUtility;
+
 
 @SpringBootTest
 class IndividualprepApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testArithmeticUtilityBean() {
+		ArithmeticUtility arithmeticUtility = new ArithmeticUtility();
+		assert arithmeticUtility != null;
+	}
+
+	@Test
+	void testVectorUtilityBean() {
+		VectorUtility vectorUtility = new VectorUtility();
+		assert vectorUtility != null;
+	}
+
+	@Test
+	void testArithmeticSubtract() {
+		ArithmeticUtility arithmeticUtility = new ArithmeticUtility();
+		double result = arithmeticUtility.subtract(10.0, 4.0);
+		assert result == 6.0;
+	}
+
+	@Test
+	void testArithmeticSubstractNegativeResult() {
+		ArithmeticUtility arithmeticUtility = new ArithmeticUtility();
+		double result = arithmeticUtility.subtract(4.0, 10.0);
+		assert result == -6.0;
 	}
 
 }
